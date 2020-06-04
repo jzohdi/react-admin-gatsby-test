2470ee0e912bcb28cd5a838aa28943fe4dc8e385
Jake Zohdi <jakez1@live.com>
2020-06-04 17:19:47 -0400

    tried seeing if data provider was issue, tried adding gatsby node html catch

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,24 +1,10 @@
 import React from "react"
-import { Admin, Resource, ListGuesser, fetchUtils } from "react-admin"
-import jsonServerProvider from "ra-data-json-server"
+import { Admin, Resource, ListGuesser } from "react-admin"
+import provider from "../dataProvider"
 
-// https://stackoverflow.com/questions/47245456/error-the-content-range-header-is-missing-in-the-http-response
-const fetchJson = (url, options = {}) => {
-  if (!options.headers) {
-    options.headers = new Headers({ Accept: "application/json" })
-  }
-  // add your own headers here
-  options.headers.set("X-Custom-Header", "foobar")
-  return fetchUtils.fetchJson(url, options)
-}
-
-const dataProvider = jsonServerProvider(
-  "https://jsonplaceholder.typicode.com",
-  fetchJson
-)
 const IndexPage = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+  <Admin dataProvider={provider}>
+    <Resource name="posts" list={ListGuesser} />
   </Admin>
 )
 
