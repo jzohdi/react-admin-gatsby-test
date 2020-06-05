13e8306431b863eba9bd7499c4922d86a9a21371
Jacob Zohdi <31998568+jzohdi@users.noreply.github.com>
2020-06-05 00:21:35 -0400

    Update admin.js

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { ListGuesser } from "react-admin"
+import { Admin } from "react-admin"
 import { Resource } from "react-admin"
 import provider from "../dataProvider"
 import loadable from "@loadable/component"
 
 // will get import desctructuring soon https://github.com/gregberge/loadable-components/pull/483
-const Admin = loadable(() => import("../components/Admin"))
+// const Admin = loadable(() => import("../components/Admin"))
 // const Resource = loadable(() => import("../components/Resource"))
 
 const IndexPage = () => (
